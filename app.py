464310a0a0930990c7cfab831bee3df43eb2ed37
Mann <gargmann963@gmail.com>
2026-03-07 06:47:18 +0530

    Setup SQLAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.db"
+
+db = SQLAlchemy(app)
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    students = Student.query.all()
+    return render_template("index.html", students=students)
+
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
